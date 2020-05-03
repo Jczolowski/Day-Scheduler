1a7f40f6500f7a5f27a68f874521c712032a990c
Justin Czolowski <Jczolowski@yahoo.com>
2020-05-03 14:12:15 -0700

    minor tweaks

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,47 @@
-
 $(document).ready(function () {
-    function getCurrentDay() {
-        const currentDay = moment().format("dddd, MMMM Do");
-        console.log(currentDay)
-        $("#currentDay").text(currentDay);
-    }
-    getCurrentDay();
+
+getCurrentDay();
+
+pastPresentFuture();
+
+
+function pastPresentFuture() {
+// get current hour
+const currentHour = moment().hours();
+
+
+
+//target description tag
+const descriptions = $(".description");
+
+
+//get current time that is stored on description block
+const currentTimeOfBlock = parseInt($(descriptions[0]).attr("data-time"));
+
+
+if(currentTimeOfBlock < currentHour) {
+    $(descriptions[0]).addClass("past");
+ }
+else if (currentTimeOfBlock == currentHour){
+    $(descriptions[0]).addClass("present");
+}
+else {
+    $(descriptions[0]).addClass("future");
+
+}
+
+}
+
+
+
+function getCurrentDay() {
+
+//get the current day formatted
+const currentDay = moment().format("dddd. MMMM Do");
+
+//set the text of the current day element
+$("#currentDay").text(currentDay);
+
+}
+
 })
-
-
